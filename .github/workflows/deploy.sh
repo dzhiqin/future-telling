06b7cc93b7838b3bcccd70aa06c7c5cc0deb59e3
Kim <dzhiqin@126.com>
2022-09-19 16:15:02 +0800

    fix: 修改CI/CD条件

--- .github/workflows/deploy.sh
+++
@@ -1,7 +0,0 @@
-
-docker pull dongzhiqin/future-telling:latest
-docker stop future-telling
-docker rm future-telling
-docker run --name future-telling -d -p 8081:80 dongzhiqin/future-telling:latest
-
-
